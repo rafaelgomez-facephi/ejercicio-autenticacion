59005e88f1c0a59a84eca52c2276fb7b5969ea3e
rafaelgomez <rafaelgomez@facephi.com>
2022-11-22 15:58:37 +0100

    [ej_01] feat: funcionando ok

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const path = require('path');
+const { auth } = require('express-oauth2-jwt-bearer');
 
 const app = express()
 const port = 3000
+const checkJwt = auth({
+  audience: 'https://test1',
+  issuerBaseURL: `https://dev-fprafa.eu.auth0.com/`,
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './web/index.html'));
 })
 
-app.get('/api/protectedData', (req, res) => {
+app.get('/api/protectedData', checkJwt, (req, res) => {
   res.send('this is important data')
 })
 
