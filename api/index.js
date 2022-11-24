27d44a1b4ac60598cb57ef7237ed1a896359e371
rafaelgomez <rafaelgomez@facephi.com>
2022-11-24 11:18:35 +0100

    first commit

---
+++ api/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path');
+
+const app = express()
+const port = 3003
+
+
+app.get('/api/protectedData', (req, res) => {
+  res.send('this is important data')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
